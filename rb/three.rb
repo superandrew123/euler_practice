c1f37e828ef02268febc9f02ae2b928e56155178
superandrew123 <andrewmiksch@gmail.com>
2016-09-14 15:59:59 -0400

    3 done in ruby

---
+++ rb/three.rb
@@ -0,0 +1,32 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+require "pry"
+
+class Integer
+    def factors
+        factors = []
+        (1..Math.sqrt(self)).each {|num|
+            if self % num == 0 && num != 1 && num != self
+                factors.push(num) unless factors.include?(num)
+                factors.push(self / num) unless factors.include?(self / num)
+            end
+        }
+        factors.sort
+    end
+    def is_prime?
+        self.factors.length == 0 ? true : false
+    end
+    def largest_prime_factor
+        factors = self.factors.reverse
+        factors.each { |factor|
+            if factor.is_prime?
+                binding
+                return factor
+            end
+        }
+    end
+end
+
+puts "#{600851475143.largest_prime_factor} is the largest factor"
+
